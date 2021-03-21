e370738fd2d9197c78c2e858eb6c180de7e7220f
erkebulanbolatov <190103187@stu.sdu.edu.kz>
2021-03-21 13:15:49 +0600

    I create /posts Api and /posts/details Api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    public function index(Request $request) {
+    	$posts = Post::all();
+
+    	return response($posts, 200);
+    }
+
+    public function get_posts(Request $request) {
+    	$posts = Post::find($request->id);
+
+    	if($posts == null) {
+    		return response(['message' => 'There is no post', 404]);
+    	}
+
+    	return response($posts, 200);
+    }
+}
